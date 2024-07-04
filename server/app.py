8066c5defda221713f80dd0358d8ac600e0e11b5
Victor Wangari <victorwangari65187@gmail.com>
2024-07-04 14:13:59 +0300

    initial commit

---
+++ server/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, make_response, jsonify
+from flask_cors import CORS
+from flask_migrate import Migrate
+
+from models import db, Movie
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+CORS(app)
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+@app.route('/movies', methods=['GET'])
+def movies():
+    if request.method == 'GET':
+        movies = Movie.query.all()
+        return make_response(jsonify([movies.to_dict() for movie in movies] ), 200,)
+    
+    return make_response (
+        jsonify({"text":"Method Not Allowed"}),
+    )
+
+if __name__ == '__main__':
+    app.run(port=5555)
